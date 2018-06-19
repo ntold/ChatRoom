e03252ec8e55d3f0740696f1350038b160a28994
ntold <nico.berchtold@me.com>
2018-06-19 10:18:39 +0200

    .

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,52 +1,51 @@
 <template>
-  <nav>
-      <div class="nav-wrapper #424242 grey darken-3">
-          <div class="container">
-            <router-link to="/" class="brand-logo left animated bounceInLeft"  style="text-decoration:none;"><i class="material-icons">chat</i> Olek</router-link>
-              <ul class="right">
-                <li v-if="isLoggedIn" class="nickname"> {{ currentUser }} </li>
-                <li v-if="isLoggedIn"><button v-on:click="logout" class="btn orange darken-4">Logout</button></li>
-              </ul>
-          </div>
-      </div>
-  </nav>
+    <div class="navbar-fixed">
+        <nav>
+            <div class="nav-wrapper #424242 grey darken-3">
+                <div class="container">
+                    <router-link to="/" class="brand-logo left animated bounceInLeft"  style="text-decoration:none;"><i class="material-icons">chat</i> Olek</router-link>
+                    <ul class="right">
+                        <li v-if="isLoggedIn" class="nickname"> {{ currentUser }} </li>
+                        <li v-if="isLoggedIn"><button v-on:click="logout" class="btn orange darken-4">Logout</button></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
 </template>
 
 <script>
-    import firebase from 'firebase';
+import firebase from "firebase";
 
-    export default {
-    name: 'navbar',
-    data() {
-        return {
-        isLoggedIn: false,
-        currentUser: false
-        };
-    },
-    created() {
-        if (firebase.auth().currentUser) {
-        this.isLoggedIn = true;
-        this.currentUser = firebase.auth().currentUser.email;
-        }
-    },
-    methods: {
-        logout: function() {
-            firebase
-                .auth()
-                .signOut()
-                .then(() => {
-                this.$router.go({ path: this.$router.path });
-                });        
-        }
-    }
+export default {
+  name: "navbar",
+  data() {
+    return {
+      isLoggedIn: false,
+      currentUser: false
     };
-
+  },
+  created() {
+    if (firebase.auth().currentUser) {
+      this.isLoggedIn = true;
+      this.currentUser = firebase.auth().currentUser.email;
+    }
+  },
+  methods: {
+    logout: function() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          this.$router.go({ path: this.$router.path });
+        });
+    }
+  }
+};
 </script>
 
 <style scoped>
-
-.nickname{
-    margin-right: 20px;
+.nickname {
+  margin-right: 20px;
 }
-
 </style>
