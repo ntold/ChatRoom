95e0dddec0b132f97ecb644ff73eb4648b1384c5
ntold <nico.berchtold@me.com>
2018-08-16 15:27:01 +0200

    nothing

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <div id="app">
-    <Navbar />
-     <router-view/>
-  </div>
+<div id="app">
+  <Navbar />
+  <router-view/>
+</div>
 </template>
 
 <script>
@@ -10,7 +10,7 @@ import Navbar from './components/Navbar';
 
 export default {
   name: 'App',
-  components:{
+  components: {
     Navbar
   }
 }
